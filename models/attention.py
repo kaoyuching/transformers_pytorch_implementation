124668c412acc333c851d3d6890b3d1a3082ef0a
kaoyuching <doris830426@gmail.com>
2024-08-12 15:44:11 +0800

    ADD: add encoder and decoder blocks

--- models/attention.py
+++ models/attention.py
@@ -2,6 +2,8 @@ from typing import Optional
 import torch
 import torch.nn as nn
 
+from models.positional_embedding import PositionalEncodeing
+
 
 class SelfAttention(nn.Module):
     def __init__(self, emb_dim: int, hidden_dim: int):
@@ -52,7 +54,7 @@ class MultiHeadAttention(nn.Module):
         self.attn = SelfAttention(emb_dim, hidden_dim)
         self.linear = nn.Linear(hidden_dim, hidden_dim)
 
-    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, attn_mask: Optional[torch.Tensor]):
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, attn_mask: Optional[torch.Tensor] = None):
         r"""
         (q, k, v) shape: (batch, seq_len, emb_dim) -> (batch, seq_len, num_heads, emb_dim/num_heads)
         """
@@ -64,3 +66,114 @@ class MultiHeadAttention(nn.Module):
         bs, seq_len = attn.shape[0, 1]
         output = self.linear(attn.reshape(bs, seq_len, -1))  # (batch, seq_len, emb_dim)
         return output
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, emb_dim: int, num_heads: int):
+        super().__init__()
+        self.multi_head_attn = MultiHeadAttention(emb_dim, emb_dim, num_heads)
+        # feed-forward network
+        self.mlp = nn.ModuleList([
+            nn.Linear(emb_dim, emb_dim),
+            nn.ReLU(),
+            nn.Linear(emb_dim, emb_dim),
+        ])
+        self.norm_attn = nn.LayerNorm(emb_dim)
+        self.norm_mlp = nn.LayerNorm(emb_dim)
+
+    def forward(self, x: torch.Tensor):
+        attn_x = self.multi_head_attn(x, x, x, attn_mask=None)
+        x = self.norm_attn(x + attn_x)
+
+        mlp_x = torch.clone(x)
+        for mlp_layer in self.mlp:
+            mlp_x = mlp_layer(mlp_x)
+        x = self.norm_mlp(x + mlp_x)
+        return x
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, emb_dim: int, num_heads: int):
+        super().__init__()
+        self.mask_multi_head_attn = MultiHeadAttention(emb_dim, emb_dim, num_heads)
+        self.multi_head_attn = MultiHeadAttention(emb_dim, emb_dim, num_heads)
+        # feed-forward network
+        self.mlp = nn.ModuleList([
+            nn.Linear(emb_dim, emb_dim),
+            nn.ReLU(),
+            nn.Linear(emb_dim, emb_dim),
+        ])
+        self.norm_mask_attn = nn.LayerNorm(emb_dim)
+        self.norm_attn = nn.LayerNorm(emb_dim)
+        self.norm_mlp = nn.LayerNorm(emb_dim)
+
+    def forward(self, x: torch.Tensor, encode_k, encode_v, attn_mask: Optional[torch.tensor] = None):
+        mask_attn_x = self.mask_multi_head_attn(x, x, x, attn_mask=attn_mask)
+        x = self.norm_mask_attn(x + mask_attn_x) # next input q
+
+        attn_x = self.multi_head_attn(x, encode_k, encode_v, attn_mask=None)
+        x = self.norm_attn(x + attn_x) # next input q
+
+        mlp_x = torch.clone(x)
+        for mlp_layer in self.mlp:
+            mlp_x = mlp_layer(mlp_x)
+        x = self.norm_mlp(x + mlp_x)
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(
+        self,
+        num_emb: int,
+        emb_dim: int,
+        num_heads: int,
+        emb_max_norm: bool = True,
+        n_blocks: int = 6,
+    ):
+        super().__init__()
+        self.input_embedding = nn.Embedding(num_emb, emb_dim, max_norm=emb_max_norm)
+        self.pos_encoding = PositionalEncodeing()
+        self.encoder_blocks = nn.ModuleList([
+            EncoderBlock(emb_dim, num_heads) for i in range(n_blocks)
+        ])
+
+    def forward(self, x: torch.Tensor):
+        # input > emb > add pos_encoding > multi-head attn > MLP
+        x_emb = self.input_embedding(x)
+        pe = self.pos_encoding(x)
+        x_emb = x_emb + pe
+
+        x_block = torch.clone(x_emb)
+        for block in self.encoder_blocks:
+            x_block = block(x_block)
+        return x_block
+
+
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        num_emb: int,
+        emb_dim: int,
+        num_heads: int,
+        emb_max_norm: bool = True,
+        n_blocks: int = 6,
+    ):
+        super().__init__()
+        self.input_embedding = nn.Embedding(num_emb, emb_dim, max_norm=emb_max_norm)
+        self.pos_encoding = PositionalEncodeing()
+        self.decoder_blocks = nn.ModuleList([
+            DecoderBlock(emb_dim, num_heads) for i in range(n_blocks)
+        ])
+        self.linear = nn.Linear(emb_dim, emb_dim)
+        self.softmax = nn.Softmax(emb_dim)
+
+    def forward(self, x: torch.Tensor, encode_k, encode_v, attn_mask: Optional[torch.tensor] = None):
+        x_emb = self.input_embedding(x)
+        pe = self.pos_encoding(x)
+        x_emb = x_emb + pe
+
+        x_block = torch.clone(x_emb)
+        for block in self.decoder_blocks:
+            x_block = block(x_block, encode_k, encode_v, attn_mask=attn_mask)
+        output = self.linear(x_block)
+        return self.softmax(output)
